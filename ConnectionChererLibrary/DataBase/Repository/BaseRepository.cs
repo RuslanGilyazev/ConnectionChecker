2a6140fc4343636826b6fa0bfe31bece61f6dbec
Ruslan Gilyazev <syswow128@gmail.com>
2019-07-21 14:45:11 +0300

    Added repository documentation

--- ConnectionChererLibrary/DataBase/Repository/BaseRepository.cs
+++ ConnectionChererLibrary/DataBase/Repository/BaseRepository.cs
@@ -1,53 +1,149 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Data.Entity;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConnectionCheckerLibrary.DataBase.Repository
 {
+    /// <summary>
+    /// The BaseRepository interface.
+    /// </summary>
+    /// <typeparam name="T">
+    /// </typeparam>
     public interface IBaseRepository<T>
     {
+        /// <summary>
+        /// The get all.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="IEnumerable"/>.
+        /// </returns>
         IEnumerable<T> GetAll();
 
+        /// <summary>
+        /// The get by id.
+        /// </summary>
+        /// <param name="key">
+        /// The key.
+        /// </param>
+        /// <returns>
+        /// The <see cref="T"/>.
+        /// </returns>
         T GetById(params object[] key);
 
+        /// <summary>
+        /// The insert.
+        /// </summary>
+        /// <param name="newInstance">
+        /// The new instance.
+        /// </param>
         void Insert(T newInstance);
 
+        /// <summary>
+        /// The update.
+        /// </summary>
+        /// <param name="existInstance">
+        /// The exist instance.
+        /// </param>
         void Update(T existInstance);
 
+        /// <summary>
+        /// The delete.
+        /// </summary>
+        /// <param name="key">
+        /// The key.
+        /// </param>
         void Delete(params object[] key);
 
+        /// <summary>
+        /// The save.
+        /// </summary>
         void Save();
     }
 
-    public abstract class BaseRepository<T> : IBaseRepository<T>
+    /// <summary>
+    /// The base repository.
+    /// </summary>
+    /// <typeparam name="T">
+    /// </typeparam>
+    public abstract class BaseRepository<T> : DbSet<T>, IBaseRepository<T>
     {
+        /// <summary>
+        /// The _db context.
+        /// </summary>
         private readonly DbContext _dbContext;
 
+        /// <summary>
+        /// The db context.
+        /// </summary>
         public DbContext DbContext => _dbContext;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BaseRepository{T}"/> class.
+        /// </summary>
+        /// <param name="context">
+        /// The context.
+        /// </param>
         protected BaseRepository(DbContext context)
         {
             _dbContext = context;
         }
 
+        /// <summary>
+        /// The get all.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="IEnumerable"/>.
+        /// </returns>
         public abstract IEnumerable<T> GetAll();
 
+        /// <summary>
+        /// The get by id.
+        /// </summary>
+        /// <param name="key">
+        /// The key.
+        /// </param>
+        /// <returns>
+        /// The <see cref="T"/>.
+        /// </returns>
         public abstract T GetById(params object[] key);
 
+        /// <summary>
+        /// The insert.
+        /// </summary>
+        /// <param name="newInstance">
+        /// The new instance.
+        /// </param>
         public abstract void Insert(T newInstance);
 
+        /// <summary>
+        /// The update.
+        /// </summary>
+        /// <param name="existInstance">
+        /// The exist instance.
+        /// </param>
         public abstract void Update(T existInstance);
 
+        /// <summary>
+        /// The delete.
+        /// </summary>
+        /// <param name="key">
+        /// The key.
+        /// </param>
         public abstract void Delete(params object[] key);
 
+        /// <summary>
+        /// The save.
+        /// </summary>
         public void Save()
         {
             _dbContext.SaveChanges();
         }
 
+        /// <summary>
+        /// The dispose.
+        /// </summary>
+        /// <param name="disposing">
+        /// The disposing.
+        /// </param>
         protected virtual void Dispose(bool disposing)
         {
             _dbContext.Dispose();
--- ConnectionChererLibrary/DataBase/Repository/ConnectionRepository.cs
+++ ConnectionChererLibrary/DataBase/Repository/ConnectionRepository.cs
@@ -1,46 +1,91 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Data.Entity;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 using ConnectionCheckerLibrary.DataBase.Context;
 using ConnectionCheckerLibrary.DataBase.Models;
 
 namespace ConnectionCheckerLibrary.DataBase.Repository
 {
+    /// <summary>
+    /// The connection repository.
+    /// </summary>
     public class ConnectionRepository : BaseRepository<Connection>
     {
+        /// <summary>
+        /// The db context.
+        /// </summary>
         public new ConnectionCheckerDbContext DbContext => (ConnectionCheckerDbContext) base.DbContext;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConnectionRepository"/> class.
+        /// </summary>
         public ConnectionRepository() : base(new ConnectionCheckerDbContext())
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConnectionRepository"/> class.
+        /// </summary>
+        /// <param name="context">
+        /// The context.
+        /// </param>
         public ConnectionRepository(ConnectionCheckerDbContext context) : base(context)
         {
         }
 
+        /// <summary>
+        /// The get all.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="IEnumerable"/>.
+        /// </returns>
         public override IEnumerable<Connection> GetAll()
         {
             return DbContext.Connection;
         }
 
+        /// <summary>
+        /// The get by id.
+        /// </summary>
+        /// <param name="key">
+        /// The key.
+        /// </param>
+        /// <returns>
+        /// The <see cref="Connection"/>.
+        /// </returns>
         public override Connection GetById(params object[] key)
         {
             return DbContext.Connection.Find(key);
         }
-         
+
+        /// <summary>
+        /// The insert.
+        /// </summary>
+        /// <param name="newInstance">
+        /// The new instance.
+        /// </param>
         public override void Insert(Connection newInstance)
         {
             DbContext.Connection.Add(newInstance);
         }
 
+        /// <summary>
+        /// The update.
+        /// </summary>
+        /// <param name="existInstance">
+        /// The exist instance.
+        /// </param>
         public override void Update(Connection existInstance)
         {
             DbContext.Entry(existInstance).State = EntityState.Modified;
         }
 
+        /// <summary>
+        /// The delete.
+        /// </summary>
+        /// <param name="key">
+        /// The key.
+        /// </param>
         public override void Delete(params object[] key)
         {
             Connection connection = GetById(key);
